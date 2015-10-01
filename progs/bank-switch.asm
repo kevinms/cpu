8989734f5a8070aa1a5ae834b9febb3cbe5087a6
Kevin M Smith <ksmith.nop@gmail.com>
2015-10-01 17:18:34 -0400

    notes

--- progs/bank-switch.asm
+++ progs/bank-switch.asm
@@ -6,6 +6,22 @@ mov r0 1
 mov r1 1
 add r0 r0 r1
 
+; Two switchable memory bank areas.
+; The banks could be used for anything. The software gets to
+; decide how it uses them. You could execute instructions from
+; both and use it as a stack. It's all up to the program.
+;
+; One possible configuration would be:
+;   #1 Memory reads and writes.
+;   #2 Instructions.
+;
+; This would allow your cpu to execute instructions from one region
+; while reading memory from another with infrequent bank switches.
+
+; There would also be a non-switchabe memory region for any code
+; that needs to be always accessable i.e. helper functions,
+; interrupt handlers, the stack, etc.
+
 ; call a function from one switchable bank to another
 ;   - store current bn and ra on the stack
 ;   - set a next-bank register
